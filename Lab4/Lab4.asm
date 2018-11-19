70f1e4ac6c41321d00942a9065dfb6dc18716faa
jshen30 <jshen30@ucsc.edu>
2018-11-18 22:08:07 -0800

    Lab4a added

---
+++ Lab4/Lab4.asm
@@ -0,0 +1,38 @@
+###########################################################################
+# Created by: Shen, Jinghao
+#             jshen30
+#             18 Nov, 2018
+#
+# Assignment: Lab 4
+#             CMPE 012, Computer Systems and Assembly Language
+#             UC Santa Cruz, Fall 2018
+#
+# Description:
+#
+# Notes:
+###########################################################################
+
+#pseudocode:
+# read the input from the program arguments
+# if numbers is not between -64 and 63 
+# 	print "invalid numbers"
+# 	return
+# else
+# 	print out the numbers
+# 	covert the numbers into 32-bit two's complement numbers
+# 	$s1 = first number
+#	$s2 = second number
+#	$s0 = first number + second number
+#	print out the sum as decimal
+#	covert the sum into 32-bit two's complement number
+#	print out the sum as 32-bit two's complement number
+#
+#	if first bit of the sum is 1
+#		print out "-" and space in Morse code
+#		
+#	print the value of the sum in Morse code
+# end if	
+
+
+
+
---
+++ Lab4/README.txt
@@ -0,0 +1,20 @@
+------------------------
+
+Lab 4: ASCII Decimal to 2SC
+CMPE 012 Fall 2018
+Shen Jinghao
+jshen30
+
+-------------------------
+What was your approach to converting each decimal number to two's complement
+form?
+Write the answer here.
+
+What did you learn in this lab?
+Write the answer here.
+
+Did you encounter any issues? Were there parts of this lab you found enjoyable?
+Write the answer here.
+
+How would you redesign this lab to make it better?
+Write the answer here.
